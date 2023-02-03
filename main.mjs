b0822d40762265ad8d7671f24fb76964d1df702b
Boris <Borismulder@outlook.com>
2023-02-03 22:25:35 +0100

    lel

--- main.mjs
+++ main.mjs
@@ -1,77 +1,62 @@
 var customFormat = {
     name: "C Export",
     extension: "cbin",
+    outputFiles: function(map, fileName){
+        if(map == null) return;
 
-    write: function(map, fileName) {
-        try{
-            var file = new TextFile(fileName, TextFile.WriteOnly);
-            // Map info
-            file.writeLine(`${map.width} ${map.height} ${map.tileWidth} ${map.tileHeight}`)
-            
-            if(map.isTileset)
-            {
-                tiled.alert("todo")
-                return;
-            }
-    
-            // Reverse order, i want object map layer first
-            for (var i = map.layerCount-1; i >= 0; --i) {
-                var layer = map.layerAt(i);
-    
-                // Objects
-                if(layer.isObjectLayer)
-                {
-                    file.writeLine("map_obj");
-                    for(var i = 0; i < layer.objects.length; i++)
-                    {
-                        var object = layer.objects[i];
-                        if(object.name.length > 16)
-                        {
-                            tiled.alert(`${object.name} length is longer than 16 characters!`);
-                            break;
-                        }
-                        file.writeLine(`${object.name} ${object.x} ${object.y}`);
-                    }
-                    file.writeLine("map_obj_end")
-                }
-                
-                // Tiles
-                if (layer.isTileLayer) {
-                    file.writeLine("map_tile");
-                    for (var y = 0; y < layer.height; ++y) {
-                        for (var x = 0; x < layer.width; ++x)
-                        {
-                            var tile = layer.tileAt(x,y);
-                            var cell = layer.cellAt(x,y);
-                            var tileProps;
-                            var dataString = `${cell.tileId} ${x} ${y}`;
-                            
-                            if(tile != null)
-                            {
-                                tileProps = tile.properties();
-                                dataString += `${tileProps.static ? ' 1' : ' 0'}`;    
-                            }
-                            else
-                            {
-                                dataString += ` 0`;
-                            }
-    
-                            file.writeLine(dataString)
-                        }
-                    }
-                    file.writeLine("map_tile_end")
-                }
-            }
-            file.commit("map_end");
-            file.commit();
-        }
-        catch(e)
+        var mapData = `${map.width} ${map.height} ${map.tileWidth} ${map.tileHeight}\n`;
+        var tileData = 'tile_data\n';
+        var objectData = 'object_data\n'
+        
+        for(var i = 0; i < map.layerCount; i++)
         {
-            tiled.error(e)
+            var layer = map.layerAt(i);
+
+            if(layer.isObjectLayer)
+            {
+                for(var j = 0; j < layer.objectCount; j++)
+                {
+                    var object = layer.objectAt(j);
+
+                    if(object.name.lenght > 16)
+                    {
+                        tiled.error(`${object.name} is longer than 16 characters!`);
+                        break;
+                    }
+                    objectData += `${object.name} ${object.x} ${object.y}\n`;
+                }
+            }
+
+            if(layer.isTileLayer)
+            {
+                for (var y = 0; y < layer.height; ++y) 
+                {
+                    for (var x = 0; x < layer.width; ++x)
+                    {
+                        var tile = layer.tileAt(x,y);
+                        if(!tile)
+                        {
+                            // add static body instead? to block player
+                            continue;
+                        }
+
+                        tileData += `${tile.id} ${x} ${y}\n`;
+                    }
+                }
+            }
         }
+        tileData += 'tile_data_end\n';
+        objectData += 'object_data_end\n';
+
+        var file = new TextFile(fileName,TextFile.WriteOnly);
+        file.write(mapData)
+        file.write(objectData)
+        file.write(tileData)
+        file.write(`${tiled.version}`);
+        file.commit()
+        file.close()
     },
+    write: function(){}
 }
 
-tiled.registerMapFormat("CBIN map format", customFormat)
-
-tiled.registerTilesetFormat("CBIN tileset format",customFormat)
+tiled.registerMapFormat("CBIN map format", customFormat);
